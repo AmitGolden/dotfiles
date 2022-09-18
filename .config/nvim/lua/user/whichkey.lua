4eab6ba5a3d783454673aa484316eb71de0426ae
Amit Goldenberg <amit.wifi@gmail.com>
2022-09-19 02:16:47 +0300

    update

--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -204,7 +204,6 @@ local mappings = {
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-		t = { "<cmd>ToggleTerm direction=tab<cr>", "Vertical" },
 	},
 }
 
