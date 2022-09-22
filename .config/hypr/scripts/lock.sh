df2a02967c8c7e8e6032bb499bfc21144c59f411
Amit Goldenberg <amit.wifi@gmail.com>
2022-09-22 21:50:54 +0300

    update

--- .config/hypr/scripts/lock.sh
+++ .config/hypr/scripts/lock.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 swaylock \
-	--screenshots \
-	--clock \
   --hide-keyboard-layout \
-	--indicator \
 	--indicator-radius 100 \
 	--indicator-thickness 7 \
-	--effect-blur 7x5 \
-	--effect-vignette 0.5:0.5 \
 	--ring-color cba6f7 \
   --ring-ver-color 89b4fa \
   --ring-wrong-color f38ba8 \
@@ -25,5 +20,11 @@ swaylock \
   --inside-ver-color 89b4fa\
   --inside-wrong-color f38ba8 \
   --inside-clear-color a6e3a1 \
-	--grace 2 \
-	--fade-in 0.2
+	--color 00000000 \
+	--clock \
+	--indicator \
+	# --grace 2 \
+	# --fade-in 0.2 \
+	# --effect-blur 7x5 \
+	# --screenshots \
+	# --effect-vignette 0.5:0.5 \
